5562926eb2c42ec476103b2fd99d441c1dcfc6a5
Ahana <sendatahana@gmail.com>
2025-04-10 23:12:39 +0530

    refactor

--- mysite/unmasque/test/regression/BaseTestCase.py
+++ mysite/unmasque/test/regression/BaseTestCase.py
@@ -3,6 +3,8 @@ import signal
 import sys
 import unittest
 
+from sympy import nsimplify, collect, Symbol, ImmutableDenseNDimArray
+
 from ...src.util.ConnectionFactory import ConnectionHelperFactory
 
 
@@ -23,6 +25,23 @@ class BaseTestCase(unittest.TestCase):
         signal.signal(signal.SIGTERM, signal_handler)
         signal.signal(signal.SIGINT, signal_handler)
         #self.sanitizer.doJob()
+
+    def test_sympy(self):
+        i_current_price = Symbol('i_current_price')
+        final_res = ImmutableDenseNDimArray([1.0 * i_current_price])
+        local_symbol_list = [i_current_price]
+        print("final_res: ", final_res)
+        print("local_symbol_list: ", local_symbol_list)
+        res_expr = final_res.applyfunc(lambda expr: nsimplify(collect(expr, local_symbol_list)))
+        #res_expr = nsimplify(collect(final_res, local_symbol_list))
+        print(res_expr)
+
+        attribs = ['i_current_price']
+        c = 0
+        for attrib in attribs:
+            c = res_expr.count(attrib)
+            print(c)
+        self.assertTrue(c)
 
 
 if __name__ == '__main__':
