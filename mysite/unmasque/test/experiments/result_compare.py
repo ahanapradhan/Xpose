a68fef4d676ca0f67b32cb11ef80c15dd830b081
Gopinath Patel <gopinath4301@gmail.com>
2025-04-25 09:20:49 +0530

    results updated.

--- mysite/unmasque/test/experiments/result_compare.py
+++ mysite/unmasque/test/experiments/result_compare.py
@@ -1,8 +1,9 @@
 import os
 import sys
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
 from mysite.unmasque.test.experiments.utils import give_conn, load_config, BENCHMARK_SQL, XFE_DIR, \
     readline_ignoring_comments, MODEL
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
 from mysite.unmasque.test.experiments.text_to_sql import create_text2SQL_agent
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
