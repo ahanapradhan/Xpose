eb971dc7f5fff11452361ad8edc9fd4fed4fe315
ahanapradhan <sendatahana@gmail.com>
2025-05-06 08:30:10 +0530

    local

--- mysite/unmasque/test/regression/AllFlagExtractionTest.py
+++ mysite/unmasque/test/regression/AllFlagExtractionTest.py
@@ -776,6 +776,28 @@ ORDER  BY d_week_seq1;"""
         self.conn.config.detect_or = True
         self.do_test(query)
 
+    def test_Q3(self):
+        query = """SELECT dt.d_year, 
+               item.i_brand_id          brand_id, 
+               item.i_brand             brand, 
+               Sum(ss_ext_discount_amt) sum_agg 
+FROM   date_dim dt, 
+       store_sales, 
+       item 
+WHERE  dt.d_date_sk = store_sales.ss_sold_date_sk 
+       AND store_sales.ss_item_sk = item.i_item_sk 
+       AND item.i_manufact_id = 427 
+       AND dt.d_moy = 11 
+GROUP  BY dt.d_year, 
+          item.i_brand, 
+          item.i_brand_id 
+ORDER  BY dt.d_year, 
+          sum_agg DESC, 
+          brand_id
+LIMIT 100;
+"""
+        self.do_test(query)
+
     def test_Q33(self):
         query = """ 
 
