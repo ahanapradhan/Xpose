99c280af8d102e6047e99b06036dbbf59b88e7e2
Ahana <sendatahana@gmail.com>
2026-01-28 14:48:19 +0530

    Update U2_workload.sql

--- mysite/unmasque/test/U2_workload.sql
+++ mysite/unmasque/test/U2_workload.sql
@@ -1,3 +1,4 @@
+-- algebraic predicates, disjucntion
 SELECT
     o_clerk,
     AVG(2 * o_totalprice + 35.90) AS total_price,
@@ -16,6 +17,7 @@ WHERE
 GROUP BY o_clerk, c_acctbal, n_name
 ORDER BY total_price DESC, o_clerk, c_acctbal, n_name;
 
+-- algebraic predicates
 SELECT
     o_clerk,
     AVG(2 * o_totalprice + 1) AS total_price,
@@ -33,6 +35,7 @@ WHERE
 GROUP BY o_clerk, c_acctbal, n_name
 ORDER BY total_price DESC, o_clerk, c_acctbal, n_name;
 
+-- algebraic predicates
 SELECT
     o_clerk,
     SUM(c_acctbal + 2 * o_totalprice) AS total_price,
@@ -47,8 +50,8 @@ WHERE
 GROUP BY o_clerk, n_name
 ORDER BY o_clerk
 LIMIT 30;
--- AoA queries
 
+-- algebraic predicates, disjucntion
 SELECT
     o_clerk,
     SUM(c_acctbal + 3.54 * o_totalprice) AS total_price,
@@ -66,6 +69,7 @@ GROUP BY o_clerk, n_name
 ORDER BY o_clerk
 LIMIT 30;
 
+-- algebraic predicate
 SELECT
     l_shipmode,
     COUNT(*) AS count
@@ -82,6 +86,7 @@ WHERE
 GROUP BY l_shipmode
 ORDER BY l_shipmode;
 
+-- algebraic predicate
 SELECT
     c_name AS name,
     c_acctbal AS account_balance
@@ -94,8 +99,7 @@ WHERE
     AND o_orderdate BETWEEN '1998-01-01' AND '1998-01-15'
     AND o_totalprice <= c_acctbal;
 
--- union and/or Aoa
-
+-- union and/or algebraic predicate
 (SELECT
     p_partkey,
     p_name
@@ -110,6 +114,7 @@ UNION ALL
  WHERE ps_suppkey = s_suppkey
    AND ps_availqty >= 201);
 
+--  algebraic predicate
 SELECT
     l_shipmode,
     COUNT(*) AS count
@@ -126,6 +131,7 @@ WHERE
 GROUP BY l_shipmode
 ORDER BY l_shipmode ASC;
 
+--  algebraic predicate
 SELECT
     o_orderpriority,
     COUNT(*) AS order_count
@@ -138,6 +144,7 @@ WHERE
 GROUP BY o_orderpriority
 ORDER BY o_orderpriority ASC;
 
+-- union and/or algebraic predicate
 (SELECT
     p_brand,
     s_name AS o_clerk,
@@ -171,6 +178,7 @@ UNION ALL
  ORDER BY o_clerk ASC
  LIMIT 5);
 
+-- algebraic predicate
 SELECT
     o_orderkey AS l_orderkey,
     ps_availqty AS l_linenumber
@@ -188,6 +196,7 @@ WHERE
 ORDER BY l_orderkey ASC
 LIMIT 7;
 
+-- algebraic predicate
 SELECT
     s_name,
     COUNT(*) AS numwait
@@ -202,6 +211,7 @@ GROUP BY s_name
 ORDER BY numwait DESC, s_name ASC
 LIMIT 100;
 
+-- algebraic predicate
 SELECT
     l_returnflag,
     l_linestatus,
@@ -220,6 +230,7 @@ WHERE
 GROUP BY l_returnflag, l_linestatus
 ORDER BY l_returnflag ASC, l_linestatus ASC;
 
+-- union
 (SELECT
     s_suppkey,
     s_name
@@ -250,8 +261,9 @@ UNION ALL
  FROM lineitem, part
  WHERE
     l_partkey = p_partkey
-    AND l_quantity >= 35.01);
+    AND l_quantity > 35);
 
+-- union
 (SELECT
     s_suppkey AS c_custkey,
     s_name AS c_name
@@ -276,6 +288,7 @@ UNION ALL
     l_partkey = p_partkey
     AND l_quantity > 20);
 
+-- union
 (SELECT
     o_orderkey,
     l_shipdate AS o_orderdate
@@ -295,6 +308,7 @@ UNION ALL
     AND c_name LIKE '%0001248%'
     AND o_orderdate >= '1997-01-01');
 
+-- union
 (SELECT
     n_name AS name,
     SUM(s_acctbal) AS total_price
@@ -316,6 +330,7 @@ UNION ALL
  ORDER BY total_price DESC, name DESC
  LIMIT 10);
 
+-- union
 (SELECT
     l_orderkey AS key,
     l_extendedprice AS price,
@@ -336,6 +351,7 @@ UNION ALL
     AND ps_suppkey = s_suppkey
     AND ps_supplycost < 100);
 
+-- union
 (SELECT
     c_custkey AS order_id,
     COUNT(*) AS total
@@ -358,6 +374,7 @@ UNION ALL
  ORDER BY total DESC, order_id ASC
  LIMIT 10);
 
+-- union and/or algebraic predicate
 (SELECT
     s_name AS name,
     s_acctbal AS account_balance
@@ -386,7 +403,7 @@ UNION ALL
     AND o_orderdate <= '1998-01-05'
     AND o_totalprice <= c_acctbal);
 
--- Q1: FULL OUTER JOIN (non-union)
+-- FULL OUTER JOIN
 SELECT
     n_name,
     r_comment
@@ -396,7 +413,7 @@ FULL OUTER JOIN region
    AND r_name = 'AFRICA';
 
 
--- Q2: RIGHT OUTER JOIN
+-- RIGHT OUTER JOIN
 SELECT
     n_name,
     c_comment
@@ -406,7 +423,7 @@ RIGHT OUTER JOIN customer
    AND c_acctbal < 1000;
 
 
--- Q3: UNION ALL with FULL OUTER JOIN + RIGHT OUTER JOIN
+-- union, with FULL OUTER JOIN + RIGHT OUTER JOIN
 SELECT
     n_name,
     r_comment
@@ -426,7 +443,7 @@ RIGHT OUTER JOIN customer
    AND c_acctbal < 1000;
 
 
--- Q4: UNION ALL with chained OUTER JOINs
+-- union, with chained OUTER JOINs
 SELECT
     n_name,
     r_comment,
@@ -455,7 +472,7 @@ RIGHT OUTER JOIN lineitem
    AND o_orderstatus = 'F';
 
 
--- Q5: Chained RIGHT OUTER JOINs (non-union)
+-- Chained RIGHT OUTER JOINs
 SELECT
     c_name,
     o_comment,
@@ -470,7 +487,6 @@ RIGHT OUTER JOIN lineitem
    AND o_orderstatus = 'F';
 
 -- nep
-
 SELECT l_shipmode, SUM(l_extendedprice) AS revenue
 FROM lineitem
 WHERE l_shipdate >= '1994-01-01'
@@ -479,6 +495,7 @@ WHERE l_shipdate >= '1994-01-01'
 GROUP BY l_shipmode
 LIMIT 100;
 
+-- nep
 SELECT l_shipmode, COUNT(*) AS count
 FROM lineitem
 WHERE l_quantity > 20
@@ -486,6 +503,7 @@ WHERE l_quantity > 20
 GROUP BY l_shipmode
 ORDER BY l_shipmode;
 
+-- nep
 SELECT l_shipmode, SUM(l_extendedprice) AS revenue
 FROM lineitem
 WHERE l_shipdate < '1994-01-01'
@@ -495,6 +513,7 @@ WHERE l_shipdate < '1994-01-01'
 GROUP BY l_shipmode
 LIMIT 100;
 
+-- nep
 SELECT l_shipmode, SUM(l_extendedprice) AS revenue
 FROM lineitem
 WHERE l_quantity < 24
@@ -503,6 +522,7 @@ WHERE l_quantity < 24
 GROUP BY l_shipmode
 LIMIT 100;
 
+-- nep
 SELECT s_name, COUNT(*) AS numwait
 FROM supplier, lineitem, orders, nation
 WHERE s_suppkey = l_suppkey
@@ -514,6 +534,7 @@ GROUP BY s_name
 ORDER BY numwait DESC, s_name
 LIMIT 100;
 
+-- nep
 SELECT c_name, o_orderdate, o_totalprice, SUM(l_quantity)
 FROM customer, orders, lineitem
 WHERE c_phone LIKE '27-_%'
@@ -524,6 +545,7 @@ GROUP BY c_name, o_orderdate, o_totalprice
 ORDER BY o_orderdate, o_totalprice DESC
 LIMIT 100;
 
+-- nep
 SELECT ps_comment, SUM(ps_supplycost * ps_availqty) AS value
 FROM partsupp, supplier, nation
 WHERE ps_suppkey = s_suppkey
@@ -535,6 +557,7 @@ GROUP BY ps_comment
 ORDER BY value DESC
 LIMIT 100;
 
+-- nep
 SELECT *
 FROM partsupp, supplier, nation
 WHERE ps_suppkey = s_suppkey
@@ -543,31 +566,29 @@ WHERE ps_suppkey = s_suppkey
   AND ps_comment <> 'dependencies'
   AND ps_comment <> 'hello world regular mina dependencies';
 
--- OR/NEP
+-- disjunction/ nep (range disjunctions may be extracted as point valued neps)
 
--- DQ1
   select n_name, c_acctbal from nation, customer WHERE n_nationkey = c_nationkey and 
   n_nationkey IN (1, 2, 3, 5, 4, 13, 14, 15) and c_acctbal < 7000 and c_acctbal > 1000 ORDER BY c_acctbal;
 
--- DQ2
 select n_name, c_acctbal from nation, customer where n_nationkey = c_nationkey and c_nationkey > 3 and 
   n_nationkey < 20 and c_nationkey != 10 and c_acctbal < 7000 LIMIT 200;
 
--- DQ3
+
 select sum(l_extendedprice) as revenue from lineitem 
   where l_shipdate >= date '1994-01-01' and l_shipdate < date '1994-01-01' + interval '1' year 
   and (l_quantity < 10 or l_quantity between 24 and 42 or l_quantity >= 50);
 
--- DQ4
+
 select avg(ps_supplycost) as cost from part, partsupp where p_partkey = ps_partkey 
   and (p_brand = 'Brand#52' or p_brand = 'Brand#12');
 
--- DQ5
+
 select p_size, ps_suppkey, count(*) as low_line_count from part. partsupp where 
   p_partkey = ps_partkey and p_brand IN ('Brand#52', 'Brand#34', 'Brand#15') and p_container IN ('WRAP BOX', 'MED BOX') 
   GROUP BY p_size, ps_suppkey  ORDER BY ps_suppkey desc LIMIT 30);
 
--- DQ6
+
 select n_name, s_acctbal, ps_availqty  from supplier, partsupp, nation where 
   ps_suppkey=s_suppkey AND ps_supplycost < 50 and s_nationkey=n_nationkey and (n_regionkey = 1 or n_regionkey =3) ORDER BY n_name;
 
